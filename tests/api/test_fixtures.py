e731725c165ef267faf4325cad4784afedc2f81e
Anna Kodenko <anna.kod@ukr.net>
2023-07-06 19:03:24 +0300

    Pytest with Marking and Fixtures

---
+++ tests/api/test_fixtures.py
@@ -0,0 +1,8 @@
+import pytest
+@pytest.mark.check
+def test_check_name(user):
+    assert user.name == 'Anna'
+
+@pytest.mark.check
+def test_check_second_name(user):
+    assert user.second_name == 'Kodenko'
